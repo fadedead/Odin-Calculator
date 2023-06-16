681458b7bd744d96c16f0c9c888823e67d472f17
fadedead <fadedeadd@gmail.com>
2023-06-16 19:30:00 +0530

    Added: functions for math operations

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(opp1, opp2) {
+  return opp1+opp2;
+}
+
+function subtract(opp1, opp2) {
+  return opp1-opp2;
+}
+
+function multiply(opp1, opp2) {
+  return opp1*opp2;
+}
+
+function divide(opp1, opp2) {
+  return opp1/opp2;
+}
